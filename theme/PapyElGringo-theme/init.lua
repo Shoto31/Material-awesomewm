7c85aa172f778855707c9e78d2d766acdbb21de6
shoto <57732081+Shoto31@users.noreply.github.com>
2021-01-28 16:27:06 +0000

    Update init.lua

--- theme/PapyElGringo-theme/init.lua
+++ theme/PapyElGringo-theme/init.lua
@@ -11,14 +11,14 @@ theme.font = 'Hack 10'
 
 -- Primary
 theme.primary = mat_colors.indigo
-theme.primary.hue_500 = '#003f6b'
+theme.primary.hue_500 = '#0970bd'
 -- Accent
 theme.accent = mat_colors.pink
 
 -- Background
 theme.background = mat_colors.blue_grey
 
-theme.background.hue_800 = '#192933'
+theme.background.hue_800 = '#565653'
 theme.background.hue_900 = '#121e25'
 
 local awesome_overrides = function(theme)
